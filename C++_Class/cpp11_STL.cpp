dbfb5a9cc0ff141a7521de8a399954b78db175d3
rudwo4861@naver.com <rudwo4861@naver.com>
2023-11-22 15:36:02 +0900

    iterator 구현

--- C++_Class/cpp11_STL.cpp
+++ C++_Class/cpp11_STL.cpp
@@ -1,14 +1,171 @@
 #include <iostream>
-#include <string>
 using namespace std;
 
-namespace Construction_String
+namespace Before_AdaptorPattern
 {
+	template<typename T, typename U>
+	T xfind(T first, T end, U c)
+	{
+		while (first != end && *first != c)
+		{
+			first++;
+		}
+		return first;
+	}
+
+	template<typename T>
+	struct Node
+	{
+		T data;
+		Node* next;
+		Node(T data, Node* n) : data(data), next(n) {}
+	};
+
+	template<typename T>
+	class slist
+	{
+		Node<T>* head;
+
+		Node<T>* current;
+	public:
+		slist() : head(nullptr), current(nullptr) {}
+
+		void push_front(T data)
+		{
+			head = new Node<T>(data, head);
+		}
+
+		slist<T>& next()
+		{
+			current = current->next;
+			return *this;
+		}
+
+		slist<T>& begin()
+		{
+			current = head;
+			return *this;
+		}
+
+		bool end()
+		{
+			return current == 0;
+		}
+
+		T operator*()
+		{
+			return current->data;
+		}
+
+		void print()
+		{
+			Node<T>* current = head;
+			while (current)
+			{
+				cout << current->data << " ";
+				current = current->next;
+			}
+		}
+	};
+
 	int main()
 	{
-		string one("Lottery Winner!");
-		return 0;
-		//아무튼 모든 상황에 대비해 생성자가 무진장 많음
-		//범용적이다 그래서 제네릭하다.
+		slist<int> s;
+		s.push_front(10);
+		s.push_front(20);
+		s.push_front(30);
+		s.print();
+		cout << endl;
+		for (s.begin(); !s.end(); s.next()) //for문으로 순회할 수 있다.
+		{
+			cout << *s << " ";              //근데 xfind알고리즘은 형식이 맞지 않아 사용 불가능
+		}                                   //그래서 도입된 개념이 어뎁터 디자인 패턴
+		return 0;                           //많은 컨테이너들이 있는데 접근, 순회 방법이 제각각
+		                                    //이를 통일시켜보자!
 	}
+}
+namespace After_AdaptorPatern
+{
+	template<typename T>
+	struct Node
+	{
+		T data;
+		Node* next;
+		Node(T data, Node* n) : data(data), next(n) {}
+	};
+
+
+	template<typename T, typename U>
+	T xfind(T first, T end, U c)
+	{
+		while (first != end && *first != c)
+		{
+			first++;
+		}
+		return first;
+	}
+
+	template<typename T>
+	class slist_proxy
+	{
+		Node<T>* current;
+	public:
+		slist_proxy(Node<T>* p = 0) : current(p) {}
+
+		bool operator!=(const slist_proxy<T> p)
+		{
+			return current != p.current;
+		}
+
+		template<typename U>
+		bool operator!=(const U& p)
+		{
+			return current->data != p;
+		}
+
+		T& operator*()
+		{
+			return current->data;
+		}
+
+		slist_proxy<T>& operator++(int)
+		{
+			current = current->next;
+			return *this;
+		}
+		
+	};
+
+	template<typename T>
+	class slist
+	{
+		Node<T>* head;
+	public:
+		slist() : head(nullptr) {}
+
+		void push_front(const T& data)
+		{
+			head = new Node<T>(data, head);
+		}
+		
+		typedef slist_proxy<T> iterator;
+		iterator begin() { return iterator(head); }
+		iterator end() { return iterator(0); }
+	};
+
+	int main()
+	{
+		slist<int> s;
+		s.push_front(10);
+		s.push_front(20);
+		s.push_front(30);
+		slist<int>::iterator iter;
+		cout << *(xfind(s.begin(), s.end(), 40));
+		return 0;
+	}
+}
+
+int main()
+{
+	After_AdaptorPatern::main();
 }
--- C++_Class/cpp18_practice_stl.cpp
+++ C++_Class/cpp18_practice_stl.cpp
@@ -52,41 +52,41 @@ public:
 
 };
 
-int main()
-{
-
-    phonebook p;
-
-
-
-    p.input("chonnam", "111-1111");
-
-    p.input("national", "222-2222");
-
-    p.input("university", "333-3333");
-
-    p.input("c++", "444-4444");
-
-    p.input("programming", "555-5555");
-
-    p.input("and", "666-6666");
-
-    p.input("practice", "777-7777");
-
-
-
-    p.get("chonnam");
-
-    p.get("national");
-
-    p.get("university");
-
-    p.get("c++");
-
-    p.get("programming");
-
-    p.get("and");
-
-    p.get("practice");
-
-}
+//int main()
+//{
+//
+//    phonebook p;
+//
+//
+//
+//    p.input("chonnam", "111-1111");
+//
+//    p.input("national", "222-2222");
+//
+//    p.input("university", "333-3333");
+//
+//    p.input("c++", "444-4444");
+//
+//    p.input("programming", "555-5555");
+//
+//    p.input("and", "666-6666");
+//
+//    p.input("practice", "777-7777");
+//
+//
+//
+//    p.get("chonnam");
+//
+//    p.get("national");
+//
+//    p.get("university");
+//
+//    p.get("c++");
+//
+//    p.get("programming");
+//
+//    p.get("and");
+//
+//    p.get("practice");
+//
+//}
